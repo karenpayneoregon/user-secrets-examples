6db2767d1ebd80f9ec0cecdaed122b66bf177513
Karen Payne <paynekaren@comcast.net>
2025-07-18 17:11:04 -0700

    Add SampleApp3 WPF project with configuration files
    by Karen

---
+++ SampleApp3/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SecretsLibrary1;
+using SecretsModelsLibrary.Models;
+
+namespace SampleApp3;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var connectionString = SecretApplicationSettingReader
+            .Instance.ReadProperty<string>(nameof(Connectionstrings), nameof(Connectionstrings.DefaultConnection));
+
+        var mailSettings = SecretApplicationSettingReader
+            .Instance.ReadSection<MailSettings>(nameof(MailSettings));
+    }
+}
